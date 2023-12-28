981ca31fd04345ec78304af1350a0eb8d94fd7c8
MansAlien <mans012033945421@gmail.com>
2023-12-28 05:42:45 +0200

    test

--- project/settings.py
+++ project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Dg13Ff1dFf-cFC6BaB*5CaceD-d16A5B',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '17882',
     }
 }
 
